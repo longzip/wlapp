812281252fe462a8dc5cad319550c1ed05843ce4
Tien Thanh <thanhnt1093@gmail.com>
2019-08-29 11:39:41 +0700

    Test

--- server/models/productcategory.js
+++ server/models/productcategory.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const ProductCategory = sequelize.define('ProductCategory', {
-    name: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
   }, {});
   ProductCategory.associate = function(models) {
     // associations can be defined here
