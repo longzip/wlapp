8bc73acca624c789b626aaa1dd4ee33569c3d3d2
Tien Thanh <thanhnt1093@gmail.com>
2019-08-29 11:51:10 +0700

    admin

---
+++ admin/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Header from './Header';
+import Menu from './Menu';
+import Footer from './Footer';
+import Content from './Content';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Menu />
+        <Content />
+        <Footer />
+      </div>
+    )
+  }
+}
