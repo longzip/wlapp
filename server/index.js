9d4f4fd0b010b81e6b13bb6b59168da8d2ee3eb2
Long ZIP <longlv@v-auto.com.vn>
2019-09-04 11:38:44 +0700

    fix server

--- server/index.js
+++ server/index.js
@@ -1,44 +1,45 @@
-require('dotenv').config(); // Sets up dotenv as soon as our application starts
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-// const db = require('./models/index');
-
-// db.sequelize.sync({
-//   force: true
-// });
+require("dotenv").config(); // Sets up dotenv as soon as our application starts
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const router = express.Router();
 
 const environment = process.env.NODE_ENV; // development
-const stage = require('./config')[environment];
+const stage = require("./config")[environment];
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(cors());
 
 // This is equivalent to
-app.use('/', bodyParser.json());
+app.use("/", bodyParser.json());
 
-if (environment !== 'production') {
-  app.use(logger('dev'));
+if (environment !== "production") {
+  app.use(logger("dev"));
   // and this
-  app.use('/', logger('dev'));
+  app.use("/", logger("dev"));
 }
 
-if (environment !== 'production') {
-  app.use(logger('dev'));
+if (environment !== "production") {
+  app.use(logger("dev"));
 }
 
-const routes = require('./routes/index.js');
+const routes = require("./routes/index.js");
 
-app.use('/api/v1', routes(router));
+app.use("/api/v1", routes(router));
+app.use(express.static(path.join(__dirname, "/server/public/")));
+app.get("/.*/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/server/public/index.html"))
+);
 
 const port = process.env.PORT || 5000;
 
